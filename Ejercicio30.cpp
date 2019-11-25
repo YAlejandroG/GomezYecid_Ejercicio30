54632d4cf9e41f65bc5d96d09c2ecd647d1900e5
YAlejandroG <56890882+YAlejandroG@users.noreply.github.com>
2019-11-25 10:02:32 -0500

    Update Ejercicio30.cpp

--- Ejercicio30.cpp
+++ Ejercicio30.cpp
@@ -5,25 +5,22 @@ using namespace std;
 
 double D = 1;
 double S = 1;
+double T = 1;
 double Xmin = -1;
 double Xmax = 1;
+int Nt = 450;
+int Nx = 30;
 
-int FTCS(int Nx);
+int FTCS(int Nt,int Nx,string nombre);
 
 int main(){
-    int Nx = 30;
-    FTCS(Nx);
+    string nombre = "difusion.dat";
+    FTCS(Nt,Nx,nombre);
     
     return 0;
 }
 
-int FTCS(int Nx){
-    
-    double dPsimax = 0;
-    double Psimax = 0;
-    
-    
-    
+int FTCS(int Nt,int Nx,string nombre){
     
     double **PSI = new double *[Nt+1];
     for (int i=0;i<=Nt;i++){
@@ -50,6 +47,28 @@ int FTCS(int Nx){
             PSI[i+1][j] = PSI[i][j]+D*dT*(PSI[i][j+1]-2*PSI[i][j]+PSI[i][j-1])/pow(dX,2)+dT*S;
         }
     }
+    
+    ofstream outfile;
+    outfile.open(nombre);
+    
+    double time = 0;
+    
+    for(int i=0; i<=Nt; i++){
+        outfile<<time<<"\t";
+        time += dT;
+        for(int j=0; j<=Nx; j++){
+            outfile<<PSI[i][j]<<"\t";
+        }
+        outfile<<endl;
+    }
+    
+    double x = -1;
+    outfile<<0<<"\t";
+    for(int j=0; j<=Nx; j++){
+        outfile<<x<<"\t";
+        x += dX;
+    }
+    outfile.close();
     
     return 0;
 }
